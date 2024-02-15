8fc594ae9ee8dda012699982f6ad179c3d7d9fb5
maslen4 <maslen4@uniba.sk>
2024-02-15 14:36:24 +0100

    Nahlad zadania a riesenia
    Uz sa neposiela cez url

--- src/components/assignment_preview.php
+++ src/components/assignment_preview.php
@@ -1,9 +1,15 @@
 <?php
+if (!$this->get("user", "admin") == 1){
+    return "Permission denied!";
+}
 
 $lang = ["sk", "en", "de"][$_SESSION["lang"]];
 
-$html = "<h2>" . $this->get("assignment_task") . ": " . $_GET[$lang. "_title"] . "</h2>";
-$html.= "<div>" . html_entity_decode($_GET[$lang . "_assignment"]) . "</div>";
+$assignment_id = $_GET["id"];
+$assignment = $this->database->assignment($assignment_id);
+
+$html = "<h2>" . $this->get("assignment_task") . ": " . $assignment[$lang. "_title"] . "</h2>";
+$html.= "<div>" . html_entity_decode($assignment[$lang . "_description"]) . "</div>";
 
 return  $html;
 ?>
